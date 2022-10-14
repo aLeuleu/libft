336aa82f3614f0673d0bec7af5a791672444f751
Aurelien <aurelien.levra@gmail.com>
2022-10-14 02:21:37 +0200

    asd

--- ft_putnbr.c
+++ ft_putnbr.c
@@ -1,7 +1,18 @@
 #include <unistd.h>
 
-void	ft_putnbr(int c)
+void	ft_putnbr(int n)
 {
-    
+    char    c;
+
+    if (n < 0)
+    {
+        
+    }
+    if (n > 10)
+        {
+            ft_putnbr(n/10);
+            ft_putnbr(n%10);
+        }
+    c = n + '0';
 	write(1,&c,1);
 }
