d1e0734c6e011286f9465a6c577ca9644b234b0c
Aurelien <aurelien.levra@gmail.com>
2022-10-25 00:27:51 +0200

    asdf

--- ft_memccpy.c
+++ ft_memccpy.c
@@ -24,3 +24,60 @@ void	*ft_memccpy(void *dest, const void *src, int c, size_t n)
 	}
 	return (NULL);
 }
+
+
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include "libft.h"
+
+static void		ft_print_result(char const *s)
+{
+	int		len;
+
+	len = 0;
+	while (s[len])
+		len++;
+	write(1, s, len);
+}
+
+static void		check_memccpy(void *dest, void *src, int c, int n)
+{
+	void	*membis;
+
+	membis = dest;
+	if (!(dest = ft_memccpy(dest, src, c, n)))
+	{
+		ft_print_result("NULL");
+		write(1, "\n", 1);
+		ft_print_result(membis);
+	}
+	else
+	{
+		ft_print_result(dest);
+		write(1, "\n", 1);
+		ft_print_result(membis);
+	}
+	free(membis);
+}
+
+int				main(int argc, const char *argv[])
+{
+	void	*mem;
+
+	if (!(mem = malloc(sizeof(*mem) * 30)) )
+		return (0);
+	memset(mem, 'j', 29);
+	((char*)mem)[29] = '\0';
+		check_memccpy(mem, "zyxwvutsrqponmlkjihgfedcba", 'r', 20);
+			memset(mem, 'j', 29);
+	((char*)mem)[29] = '\0';
+		check_memccpy(mem, "zyxwvutsrqponmlkjihgfedcba", 'r', 3);
+			memset(mem, 'j', 29);
+	((char*)mem)[29] = '\0';
+		check_memccpy(mem, "zyxwvutsrqponmlkjihgfedcba", 'a', 26);
+			memset(mem, 'j', 29);
+	((char*)mem)[29] = '\0';
+		check_memccpy(mem, "zyxwvuzyxwvu", 'x', 20);
+	return (0);
+}
--- ft_memcpy.c
+++ ft_memcpy.c
@@ -21,11 +21,12 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <stdio.h>
 #include "libft.h"
 
 static void		check_memcpy(void *dest, void *src, int n)
 {
-	if (dest != ftmemcpy(dest, src, n))
+	if (dest != ft_memcpy(dest, src, n))
 		write(1, "dest's adress was not returned\n", 31);
 	write(1, dest, 30);
 	//free(dest);
@@ -40,10 +41,16 @@ int				main(int argc, const char *argv[])
 		return (0);
 	memset(mem, 'j', 30);
 		check_memcpy(mem, "zyxwvutsrqponmlkjihgfedcba", 14);
+		memset(mem, 'j', 30);
+		printf("\n");
 		check_memcpy(mem, "zyxwvutst", 0);
+		memset(mem, 'j', 30);
+		printf("\n");
 		check_memcpy(mem, "zy\0xw\0vu\0\0tsr", 11);
 	return (0);
 }
 /*
-zyxwvutsrqponmjjjjjjjjjjjjjjjjzyxwvutsrqponmjjjjjjjjjjjjjjjjzyxwvutonmjjjjjjjjjjjjjjjj
+zyxwvutsrqponmjjjjjjjjjjjjjjjj
+jjjjjjjjjjjjjjjjjjjjjjjjjjjjjj
+zyxwvutjjjjjjjjjjjjjjjjjjj
  */
