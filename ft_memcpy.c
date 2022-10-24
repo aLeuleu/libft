1f455cc4d3102ba8a3cf80887500303a98b5f828
Aurelien <aurelien.levra@gmail.com>
2022-10-25 00:19:57 +0200

    asdf

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -17,3 +17,33 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 	}
 	return (dest);
 }
+
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include "libft.h"
+
+static void		check_memcpy(void *dest, void *src, int n)
+{
+	if (dest != ftmemcpy(dest, src, n))
+		write(1, "dest's adress was not returned\n", 31);
+	write(1, dest, 30);
+	//free(dest);
+}
+
+int				main(int argc, const char *argv[])
+{
+	void	*mem;
+	int		arg;
+
+	if (!(mem = malloc(sizeof(*mem) * 30)))
+		return (0);
+	memset(mem, 'j', 30);
+		check_memcpy(mem, "zyxwvutsrqponmlkjihgfedcba", 14);
+		check_memcpy(mem, "zyxwvutst", 0);
+		check_memcpy(mem, "zy\0xw\0vu\0\0tsr", 11);
+	return (0);
+}
+/*
+zyxwvutsrqponmjjjjjjjjjjjjjjjjzyxwvutsrqponmjjjjjjjjjjjjjjjjzyxwvutonmjjjjjjjjjjjjjjjj
+ */
