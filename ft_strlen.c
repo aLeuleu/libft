8c99714dd632a81294e7c3dc5ce709f6ac043c42
Aurelien <aurelien.levra@gmail.com>
2022-10-25 01:59:03 +0200

    asdf

--- ft_strlen.c
+++ ft_strlen.c
@@ -1,10 +1,51 @@
+#include <unistd.h>
 
-int ft_strlen(char *s)
+int	ft_strlen(char *s)
 {
-	int i;
+	int	i;
 
 	i = 0;
-	while(s[i])
+	while (s[i])
 		i++;
 	return (i);
 }
+
+#include "libft.h"
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <unistd.h>
+
+static void	ft_print_result(int n)
+{
+	char	c;
+
+	if (n >= 10)
+		ft_print_result(n / 10);
+	c = n % 10 + '0';
+	write(1, &c, 1);
+}
+
+int	main(int argc, const char *argv[])
+{
+	ft_print_result(ft_strlen("Hello !"));
+	printf("\n");
+	ft_print_result(ft_strlen("1"));
+	printf("\n");
+	ft_print_result(ft_strlen("lorem\tipsum\tdolor\nsit\namet\n"));
+	printf("\n");
+	ft_print_result(ft_strlen(""));
+	printf("\n");
+	ft_print_result(ft_strlen("\n\n\f\r\t"));
+	printf("\n");
+	ft_print_result(ft_strlen("   "));
+	return (0);
+}
+/*
+7
+1
+27
+0
+5
+3
+ */
