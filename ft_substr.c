1d39468dcb038a2577245cd7ee66db1d92ce7e98
aLeuleu <aurelien.levra@gmail.com>
2022-11-08 19:26:21 +0100

    asdf

--- ft_substr.c
+++ ft_substr.c
@@ -6,11 +6,18 @@
 /*   By: alevra <alevra@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/03 22:55:54 by alevra            #+#    #+#             */
-/*   Updated: 2022/11/08 19:14:51 by alevra           ###   ########lyon.fr   */
+/*   Updated: 2022/11/08 19:25:08 by alevra           ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+static int	ft_min(int a, int b)
+{
+	if (a > b)
+		return (b);
+	return (a);
+}
 
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
@@ -20,7 +27,7 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	i = 0;
 	if (len == 0 || start > ft_strlen(s))
 		return (ft_strdup(""));
-	if (!(res = (char *)calloc(len + 1, sizeof(char))))
+	if (!(res = (char *)ft_calloc(ft_min(ft_strlen(s) + 1,(int)len +1), sizeof(char))))
 		return (NULL);
 	while (len > 0 && s[start] != '\0' && i < len)
 		res[i++] = s[start++];
