014bb34d1248eebaadfc3156ff25a23bc8abab79
aLeuleu <aurelien.levra@gmail.com>
2022-11-09 23:18:34 +0100

    asdf

--- ft_split.c
+++ ft_split.c
@@ -6,43 +6,33 @@
 /*   By: alevra <alevra@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/03 23:33:49 by alevra            #+#    #+#             */
-/*   Updated: 2022/11/09 22:35:17 by alevra           ###   ########lyon.fr   */
+/*   Updated: 2022/11/09 23:15:52 by alevra           ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
 //penser a tout free en cas de probleme
-typedef struct s_list_split
-{
-	void				*content;
-	struct s_list_split	*next;
-}	t_list_split;
 
-static t_list_split	*ft_lstnew(void *a)
+/*void	freetab()
 {
-	t_list_split	*new_element;
 
-	new_element = malloc(sizeof(t_list_split));
-	if (!new_element)
-		return (NULL);
-	new_element->content = a;
-	new_element->next = NULL;
-	return (new_element);
-}
+} */
 
-static void	insert_str_into_lst(char *str, size_t	size, t_list_split *list)
+
+
+static void	insert_str_into_lst(const char *str, size_t	size, t_list *list)
 {
-	t_list_split	*new;
+	t_list			*new;
 	char			*new_str;
 
 	new_str = malloc(sizeof(char) * (size + 1));
-	ft_strlcpy(new_str, str, size);
+	ft_strlcpy(new_str, str, size + 1);
 	if (!new_str)
-		return (NULL);
+		return ; //freeTab()
 	new = ft_lstnew(new_str);
-	new->next = list;
-	list = new;
+	ft_lstadd_back(&list,new);
+	return ;
 }
 
 void	create_str_tab(void)
@@ -56,7 +46,7 @@ char	**ft_split(char const *s, char c)
 	int				start;
 	int				end;
 	int				i;
-	t_list_split	*str_list;
+	t_list			*str_list;
 
 	i = -1;
 	start = 0;
