442e066f85d07dcb9f0f02c97dd8366351308f6f
aLeuleu <aurelien.levra@gmail.com>
2022-11-22 19:45:04 +0100

    asdf

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,11 +6,23 @@
 /*   By: alevra <alevra@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/21 09:22:49 by alevra            #+#    #+#             */
-/*   Updated: 2022/11/21 11:17:56 by alevra           ###   ########lyon.fr   */
+/*   Updated: 2022/11/22 18:55:52 by alevra           ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+static t_list	*ft_lstnew2(void *a, void (*del)(void *))
+{
+	t_list	*new_element;
+
+	new_element = malloc(sizeof(t_list));
+	if (!new_element)
+		return (del(a), NULL);
+	new_element->content = a;
+	new_element->next = NULL;
+	return (new_element);
+}
 
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
@@ -22,7 +34,7 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 	first = NULL;
 	while (lst)
 	{
-		new = ft_lstnew((*f)(lst->content));
+		new = ft_lstnew2((*f)(lst->content), del);
 		if (!new)
 		{
 			ft_lstclear(&first, del);
