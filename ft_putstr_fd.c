7a0ad0e0f8d5e7a1e78fd3c4c466ef0022efc86e
aLeuleu <aurelien.levra@gmail.com>
2022-11-28 14:54:31 +0100

    _

--- ft_putstr_fd.c
+++ ft_putstr_fd.c
@@ -6,7 +6,7 @@
 /*   By: alevra <alevra@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/09 16:39:58 by alevra            #+#    #+#             */
-/*   Updated: 2022/11/28 14:53:06 by alevra           ###   ########lyon.fr   */
+/*   Updated: 2022/11/28 14:54:26 by alevra           ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,6 @@
 void	ft_putstr_fd(char const *str, int fd)
 {
 	if (!str)
-		return (ft_putstr("(null)"));
+		return (ft_putstr_fd("(null)", fd));
 	write(fd, str, ft_strlen(str));
 }
