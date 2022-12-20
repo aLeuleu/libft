facecbef8dc87abdce2a1cf5c2e3f12b5d510faa
aLeuleu <aurelien.levra@gmail.com>
2022-12-20 22:03:28 +0100

    deleted debug comands

--- ft_freetab.c
+++ ft_freetab.c
@@ -6,7 +6,7 @@
 /*   By: alevra <alevra@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/17 13:42:46 by alevra            #+#    #+#             */
-/*   Updated: 2022/12/19 18:35:03 by alevra           ###   ########lyon.fr   */
+/*   Updated: 2022/12/20 22:03:06 by alevra           ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,10 @@
 
 void	ft_freetab(void **tab, int position)
 {
-	ft_printf("f3 pos : %d\n", position);			
 	if (position >= 0)
 	{
 		while (position >= 0)
 		{
-			ft_printf("f4 pos : %d\n", position);			
-			ft_printf("%s\n", (char *)(tab[position]));
 			free(tab[position]);
 			position--;
 		}
