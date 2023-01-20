264ac87ecb32157aee68fd9234a3f29ac5408719
aLeuleu <aurelien.levra@gmail.com>
2023-01-20 15:30:47 +0100

    hextoi[WIP]

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: alevra <alevra@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/24 18:52:37 by alevra            #+#    #+#             */
-/*   Updated: 2022/11/23 10:05:00 by alevra           ###   ########lyon.fr   */
+/*   Updated: 2023/01/20 15:30:22 by alevra           ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -62,6 +62,53 @@ int	ft_atoi(const char *a)
 		if (res != ((res * 10) + (a[i] - 48)) / 10)
 			return ((sign + 1) / -2);
 		res = (res * 10) + a[i] - '0';
+		i++;
+	}
+	res *= sign;
+	return ((int)res);
+}
+
+static int	is_hexa(char c)
+{
+	int	is_number;
+	int	is_hex_letter;
+
+	is_number = (c >= '0' && c < '9');
+	is_hex_letter = (c >= 'A' && c <= 'F');
+	return (is_number || is_hex_letter);
+}
+
+static int	hex_value(char c)
+{
+	int	is_number;
+	int	is_hex_letter;
+
+	is_number = (c >= '0' && c < '9');
+	is_hex_letter = (c >= 'A' && c <= 'F');
+	if // a finir
+}
+
+int		ft_hextoi(const char *a)
+{
+	long long	res;
+	size_t		i;
+	int			sign;
+	
+	res = 0;
+	i = skip_whitespace(a);
+	sign = 1;
+	if (is_plus_or_minus(a[i]) && is_plus_or_minus(a[i + 1]))
+		return (0);
+	if (a[i] == '-')
+	{
+		sign = -1;
+		i++;
+	}
+	if (a[i] == '+')
+		i++;
+	while (is_hexa(a[i]))
+	{
+		res = (res * 16) + hex_value(a[i]);
 		i++;
 	}
 	res *= sign;
